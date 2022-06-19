d71922b058d3b0421b927ae120909af5aa40da5a
CptKibbles <107287560+CptKibbles@users.noreply.github.com>
2022-06-19 00:39:19 -0500

    Update Body Tracking with centered torso.py

--- Body Tracking with centered torso.py
+++ Body Tracking with centered torso.py
@@ -2,6 +2,9 @@ import cv2
 import mediapipe as mp
 from mediapipe.framework.formats import landmark_pb2
 import scipy.spatial
+import serial
+import time
+import serial.tools.list_ports
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -14,6 +17,8 @@ height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 width = int(width)
 height = int(height)
 print(type(width), type(height))
+
+ports = list(serial.tools.list_ports.comports())
 
 with mp_pose.Pose(
     min_detection_confidence=0.5,
@@ -50,7 +55,15 @@ with mp_pose.Pose(
       torso_center_y = ((results.pose_landmarks.landmark[11].y + results.pose_landmarks.landmark[23].y)/2 + (results.pose_landmarks.landmark[24].y + results.pose_landmarks.landmark[12].y)/2)/2
       print ("centered torso x cord: ", torso_center_x, "\ncentered torso y cord: ", torso_center_y)
       cv2.circle(image, (320, 240), 40, (0,0,244), 2)
+      
       if .4 < torso_center_x < .6 and .35 < torso_center_y < .7:
+        x = 6
+        for p in ports:
+          if 'Arduino' in p.manufacturer:
+            arduino = serial.Serial(port=p.device, baudrate=115200, timeout=.1)
+            print (p.device)
+            while True:
+              arduino.write(bytes(x, ))
         cv2.putText(image, "fire", (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
 
