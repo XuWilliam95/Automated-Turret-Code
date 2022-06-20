f240b7b63b7b279c41429c46897aefbaf9b57ba5
kujabes <106495981+kujabes@users.noreply.github.com>
2022-06-20 16:39:49 -0500

    Add files via upload

---
+++ sketch_jun18a.ino
@@ -0,0 +1,20 @@
+int x;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  while (!Serial.available());
+  x = Serial.readString().toInt();
+  // Serial.print(x + 1);
+  if (x == 6){
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(1000);                       // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(1000);
+      }
+  }
+  
